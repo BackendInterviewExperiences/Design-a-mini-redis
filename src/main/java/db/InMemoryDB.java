702403f755255049c0d816b1561b2cc79b7245e4
muskan-agarwal_568 <muskan.agarwal899@gmail.com>
2026-07-20 07:57:28 +0530

    Level 1

--- src/main/java/db/InMemoryDB.java
+++ src/main/java/db/InMemoryDB.java
@@ -22,15 +22,51 @@ public class InMemoryDB implements MiniRedis {
         return Optional.ofNullable(value);
     }
 
+    // If the key is not present, do nothing return false.
+    // If key is present but field is not present, do nothing return false
+    // if key is present, field is present but value != expectedValue return false
+    // else return true.
+    // Mental note: These are basis of lock free algorithms, as we first compare then replace we do't need to have locks.
+    // In redis - GETSET and LuaScripts serve a similar atomic compare swap purpose..
     @Override
     public boolean compareAndSet(int timestamp, String key, String field, int expectedValue, int newValue) {
-        // TODO(human): implement compareAndSet
-        return false;
+        Map<String,Integer> record = store.get(key);
+        if(record == null){
+            System.out.println("Key does not exist in store");
+            return false;
+        }
+        int value = record.get(field);
+        if(value == null){
+            System.out.println("Field does not exist in record");
+            return false;
+        }
+        if(value != expectedValue){
+            System.out.println("Value is not equal to expectedValue");
+            return false;
+        }
+        record.put(field,newValue);
+        store.put(key,record);
+        return true;
     }
 
     @Override
     public boolean compareAndDelete(int timestamp, String key, String field, int expectedValue) {
-        // TODO(human): implement compareAndDelete
-        return false;
+        Map<String,Integer> record = store.get(key);
+        if(record == null){
+            System.out.println("Key does not exist in store");
+            return false;
+        }
+        int value = record.get(field);
+        if(value == null){
+            System.out.println("Field does not exist in record");
+            return false;
+        }
+        if(value != expectedValue){
+            System.out.println("Value is not equal to expectedValue");
+            return false;
+        }
+        record.remove(field);
+        store.put(key,record);
+        return true;
     }
 }
